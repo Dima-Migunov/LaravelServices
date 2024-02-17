8ec6595fe49c4cb3402d9264703e44a5185b825a
Dmitri Migunov <dmitri.migunov@gmail.com>
2024-02-17 13:47:43 +0300

    add dependencies

---
+++ src/ResponseService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Migunov\ResponseService;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+class ResponseService
+{
+    public static function json(
+        array $data,
+        array $params = [],
+        int $status = Response::HTTP_OK
+    ): JsonResponse {
+        $meta = ['timestamp' => time()];
+
+        if (isset($data['meta'])) {
+            $meta = array_merge($meta, $data['meta']);
+            unset($data['meta']);
+        }
+
+        if ($params) {
+            $meta['params'] = $params;
+        }
+
+        if (isset($data['data'])) {
+            $d = $data['data'];
+            unset($data['data']);
+            $meta = array_merge($meta, $data);
+            $data = $d;
+        }
+
+        return response()->json(
+            [
+                'meta' => $meta,
+                'data' => $data,
+            ],
+            $status
+        );
+    }
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitf2ad3d3c0eebe15a315d013dd7b315f7
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf2ad3d3c0eebe15a315d013dd7b315f7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitf2ad3d3c0eebe15a315d013dd7b315f7', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitf2ad3d3c0eebe15a315d013dd7b315f7
         call_user_func(\Composer\Autoload\ComposerStaticInitf2ad3d3c0eebe15a315d013dd7b315f7::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitf2ad3d3c0eebe15a315d013dd7b315f7::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
