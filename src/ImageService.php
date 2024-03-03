df9faf5f6536db10a2fa2a8835ad374352038529
Dmitri Migunov <dmitri.migunov@gmail.com>
2024-03-03 20:36:32 +0300

    ImageService::isImageFile($path)

--- src/ImageService.php
+++ src/ImageService.php
@@ -6,65 +6,18 @@ use Exception;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManager;
 use Migunov\Services\Traits\WithImageFromUrlPage;
+use Migunov\Services\Traits\WithImageResize;
 
 class ImageService
 {
     use WithImageFromUrlPage;
+    use WithImageResize;
 
-    public const MAX_WIDTH = 1200;
-    public const MAX_HEIGHT = 600;
+    public const IMAGE_EXTENSIONS = ['png', 'jpeg', 'jpg', 'svg', 'gif', 'webp', 'tiff'];
 
-    public const PREVIEW_MAX_WIDTH = 600;
-    public const PREVIEW_MAX_HEIGHT = 400;
-
-    public const MINI_MAX_WIDTH = 64;
-    public const MINI_MAX_HEIGHT = 64;
-
-    public static function resize(
-        string $path,
-        int $width = 1200,
-        int $height = 600,
-        ?string $targetPath = null
-    ): void {
-        $abspath = Storage::disk('public')->path($path);
-        $target = $targetPath ? Storage::disk('public')->path($targetPath) : $abspath;
-        $image = ImageManager::gd();
-
-        try {
-            $image = $image->read($abspath);
-        } catch(Exception) {
-            return;
-        }
-
-        if ($width == 0 && $height == 0) {
-            $width = $image->width();
-            $height = $image->height();
-        }
-
-        if ($width < 0) {
-            $width = $image->width();
-        }
-
-        if ($height < 0) {
-            $height = $image->height();
-        }
-
-        if ($width > self::MAX_WIDTH) {
-            $width = self::MAX_WIDTH;
-        }
-
-        if ($height > self::MAX_HEIGHT) {
-            $height = self::MAX_HEIGHT;
-        }
-
-        if ($height == 0) {
-            $image->scale(width:$width)->save($target, 90);
-        } elseif ($width == 0) {
-            $image->scale(height:$height)->save($target, 90);
-        } elseif ($image->width() >= $image->height()) {
-            $image->scale(width:$width)->save($target, 90);
-        } else {
-            $image->scale(height:$height)->save($target, 90);
-        }
+    public static function isImageFile(string $path): bool
+    {
+        $imageExts = implode('|', self::IMAGE_EXTENSIONS);
+        return !!preg_match('#\.(' . $imageExts . ')$#', $path);
     }
 }
---
+++ src/Traits/WithImageResize.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Migunov\Services\Traits;
+
+use Exception;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManager;
+
+trait WithImageResize
+{
+    public const MAX_WIDTH = 1200;
+    public const MAX_HEIGHT = 600;
+
+    public const PREVIEW_MAX_WIDTH = 600;
+    public const PREVIEW_MAX_HEIGHT = 400;
+
+    public const MINI_MAX_WIDTH = 64;
+    public const MINI_MAX_HEIGHT = 64;
+
+    public static function resize(
+        string $path,
+        int $width = 1200,
+        int $height = 600,
+        ?string $targetPath = null
+    ): void {
+        $abspath = Storage::disk('public')->path($path);
+        $target = $targetPath ? Storage::disk('public')->path($targetPath) : $abspath;
+        $image = ImageManager::gd();
+
+        try {
+            $image = $image->read($abspath);
+        } catch(Exception) {
+            return;
+        }
+
+        if ($width == 0 && $height == 0) {
+            $width = $image->width();
+            $height = $image->height();
+        }
+
+        if ($width < 0) {
+            $width = $image->width();
+        }
+
+        if ($height < 0) {
+            $height = $image->height();
+        }
+
+        if ($width > self::MAX_WIDTH) {
+            $width = self::MAX_WIDTH;
+        }
+
+        if ($height > self::MAX_HEIGHT) {
+            $height = self::MAX_HEIGHT;
+        }
+
+        if ($height == 0) {
+            $image->scale(width:$width)->save($target, 90);
+        } elseif ($width == 0) {
+            $image->scale(height:$height)->save($target, 90);
+        } elseif ($image->width() >= $image->height()) {
+            $image->scale(width:$width)->save($target, 90);
+        } else {
+            $image->scale(height:$height)->save($target, 90);
+        }
+    }
+}
